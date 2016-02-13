5d4ae46d9b7a32d83a7f4eda720642033f35a75c
jonah sobol <jonah.sobol@gmail.com>
2016-02-12 17:02:32 -0800

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,36 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(params.require(:tweet).permit(:message, :user))
+
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+
+
+
+
+
+
+
+end
